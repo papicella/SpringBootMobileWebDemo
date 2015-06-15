da5e961489c2e3e5cce6bd389a56a548a54cb252
pasapples <pasapi@au1.ibm.com>
2015-06-15 14:00:13 +1000

    Initial commit

---
+++ src/main/java/apples/spring/mobile/GreetingController.java
@@ -0,0 +1,15 @@
+package apples.spring.mobile;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class GreetingController
+{
+
+    @RequestMapping("/")
+    public String greeting() {
+        return "greeting";
+    }
+
+}
---
+++ src/main/java/apples/spring/mobile/SpringBootMobileWebDemoApplication.java
@@ -0,0 +1,12 @@
+package apples.spring.mobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMobileWebDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMobileWebDemoApplication.class, args);
+    }
+}
